5c2ebbced7ad06f2cd827ab8467a1b9a6c395374
McKeever <P2828624@CORP.CHARTERCOM.COM>
2019-07-16 15:51:38 -0500

    Updates

---
+++ precache-manifest.05e2b9f41e355910a9ed3415fdf2dfda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58571df7584568b797d1de6be77e9b1d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4dfc15dc62d9c6187142",
+    "url": "/monsters-rolodex/static/css/main.c9c21e89.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4dfc15dc62d9c6187142",
+    "url": "/monsters-rolodex/static/js/main.306d4bd5.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
